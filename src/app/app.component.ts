8da8f46ce53fd3fd71ba8b87e1e1b52def2ed3de
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-09-30 02:08:44 +0200

    Remove local test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MyCustomComponentComponent } from './my-custom-component/my-custom-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyCustomComponentComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
